ddb529c89af3c001d5e342802c19bf04a955f676
makisvan <makisvan@outlook.com>
2024-10-07 16:29:59 +0200

    Updated code in CheckVictory class

--- CheckVictory.cs
+++ CheckVictory.cs
@@ -11,18 +11,58 @@ namespace ConsoleApp1
     {
         public bool IsVictory(string[] board) // metod som kommer användas för att kolla om någon har vunnit
         {
-        bool row1 = board[0] == board[1] && board[1] == board[2]; // Kollar rows
-        bool row2 = board[3] == board[4] && board[4] == board[5];
-        bool row3 = board[6] == board[7] && board[7] == board[8];
+            if (this.checkIfOneOfTheRowsIsFilledWithTheSameMark(board))
+                return true;
+            if (this.checkIfOneOfTheColumnsIsFilledWithTheSameMark(board))
+                return true;
+            if (this.checkIfOneOfTheDiagonalsIsFilledWithTheSameMark(board))
+                return true;
+            return false; // retunerar true om någon av dessa är true = Wins!
+        }
+        private bool isRowFilledWithTheSameMark(string cell1, string cell2, string cell3)
+        {
+            if (cell1 == cell2 && cell2 == cell3) return true;
+            return false;
+        }
+        private bool isColumnFilledWithTheSameMark(string cell1, string cell2, string cell3)
+        {
+            if (cell1 == cell2 && cell2 == cell3) return true;
+            return false;
+        }
+        private bool isDiagonalFilledWithTheSameMark(string cell1, string cell5, string cell9)
+        {
+            if (cell1 == cell5 && cell5 == cell9) return true;
+            return false;
 
-        bool col1 = board[0] == board[3] && board[3] == board[6]; // Kollar columns
-        bool col2 = board[1] == board[4] && board[4] == board[7];
-        bool col3 = board[2] == board[5] && board[5] == board[8];
+        }
+        private bool checkIfOneOfTheRowsIsFilledWithTheSameMark(string[] board)
+        {
+            if (this.isRowFilledWithTheSameMark(board[0], board[1], board[2]))
+                return true;
+            if (this.isRowFilledWithTheSameMark(board[3], board[4], board[5]))
+                return true;
+            if (this.isRowFilledWithTheSameMark(board[6], board[7], board[8]))
+                return true;
+            return false;
+        }
+        private bool checkIfOneOfTheColumnsIsFilledWithTheSameMark(string[] board)
+        {
+            if (this.isColumnFilledWithTheSameMark(board[0], board[3], board[6]))
+                return true;
+            if (this.isColumnFilledWithTheSameMark(board[1], board[4], board[7]))
+                return true;
+            if (this.isColumnFilledWithTheSameMark(board[2], board[5], board[8]))
+                return true;
+            return false;
 
-        bool diagDown = board[0] == board[4] && board[4] == board[8]; // Kollar diagonals
-        bool diagUp = board[6] == board[4] && board[4] == board[2];
-
-        return row1 || row2 || row3 || col1 || col2 || col3 || diagDown || diagUp; // retunerar true om någon av dessa är true = Wins!
+        }
+        private bool checkIfOneOfTheDiagonalsIsFilledWithTheSameMark(string[] board)
+        {
+            if (this.isDiagonalFilledWithTheSameMark(board[0], board[4], board[8]))
+                return true;
+            if (this.isDiagonalFilledWithTheSameMark(board[6], board[4], board[2]))
+                return true;
+            return false;
         }
     }
 }
